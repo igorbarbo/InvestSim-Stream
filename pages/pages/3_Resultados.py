34236465e0b243683fefcd5025e584906598fd7f
Igor Barbosa <igorbarbosapt@gmail.com>
2026-02-05 10:30:53 +0000

    Create 3_Resultados.py

---
+++ pages/pages/3_Resultados.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("📑 Resultados")
+
+st.info(
+    "Aqui futuramente entra:\n"
+    "- Gráficos\n"
+    "- Comparação com Selic\n"
+    "- Histórico mensal\n"
+    "- Dados do Yahoo Finance"
+)
+
+st.write("🚧 Em construção")
