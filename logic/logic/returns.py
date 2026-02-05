28b7434e93d20e71c2ec8ae2bc78a6895b0e98f4
Igor Barbosa <igorbarbosapt@gmail.com>
2026-02-05 21:56:58 +0000

    Delete logic directory

--- logic/logic/returns.py
+++
@@ -1,5 +0,0 @@
-def real_return(nominal: float, inflation: float) -> float:
-    """Calcula a rentabilidade real usando a Equação de Fisher."""
-    # i_real = [(1 + i_nom) / (1 + i_inf)] - 1
-    return ((1 + nominal) / (1 + inflation)) - 1
-  
