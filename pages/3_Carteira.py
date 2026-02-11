6b457d9d4dddca98e79bfec474bee436c5edad11
Igor Barbosa <igorbarbosapt@gmail.com>
2026-02-11 21:08:32 +0000

    Delete pages directory

--- pages/3_Carteira.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("📂 Minha Carteira")
-st.write("Gerencie seus ativos aqui.")
-
-# Exemplo simples de tabela
-ativos = {"Ativo": ["PETR4", "VALE3", "CDB"], "Qtd": [100, 50, 1]}
-st.table(ativos)
